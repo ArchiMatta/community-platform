abeec251d56d6673a2435cc37b6aec5762d3e7e2
ArchiMatta <archimatta100@gmail.com>
2025-06-22 19:47:05 +0530

    replace userid with token

--- client/src/components/ProtectedRoute.js
+++ client/src/components/ProtectedRoute.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Navigate } from 'react-router-dom';
 
 const ProtectedRoute = ({ children }) => {
-  const isLoggedIn = !!localStorage.getItem('userId');
+  const isLoggedIn = !!localStorage.getItem('token');
 
   return isLoggedIn ? children : <Navigate to="/login" replace />;
 };
