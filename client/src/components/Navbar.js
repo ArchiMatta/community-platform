ee193d5aae8a774b16943fb6016fd6943096e5f2
ArchiMatta <archimatta100@gmail.com>
2025-06-22 20:05:07 +0530

    Navbar Fix

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -3,7 +3,7 @@ import { Link, useNavigate } from 'react-router-dom';
 
 const Navbar = () => {
   const navigate = useNavigate();
-  const isLoggedIn = !!localStorage.getItem('userId');
+  const isLoggedIn = !!localStorage.getItem('token');
 
   const handleLogout = () => {
     localStorage.removeItem('token');
