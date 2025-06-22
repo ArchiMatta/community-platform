297886e29e37d1e204cf9ff579eb2b6d0bf33480
ArchiMatta <archimatta100@gmail.com>
2025-06-22 13:04:32 +0530

    Implement JWT authenticatio

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
+const auth = require('../middlewares/auth');
 
-router.get('/:id', userController.getUserById);
-router.put('/:id', userController.updateUser);
+// Route to get a user by ID - Protected by authentication
+router.get('/:id', auth, userController.getUserById);
+
+// Route to update a user by ID - Protected by authentication
+router.put('/:id', auth, userController.updateUser);
 
 module.exports = router;
